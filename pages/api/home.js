eb589832c99083ad085fff6d2852557ae8111470
heldinho <hfpsis25@gmail.com>
2021-02-01 16:13:05 -0300

    fix

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res
-    .status(200)
-    .json({
-      message: 'Teste de api com next.js',
-      ...req.query,
-      body: { ...req.body }
-    })
+  res.status(200).json({
+    message: 'Teste de api com next.js',
+    headers: { ...req.headers },
+    query: { ...req.query },
+    body: { ...req.body }
+  })
 }
