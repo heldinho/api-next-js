0929020203d2640968ea63d4088d667b804c3694
heldinho <hfpsis25@gmail.com>
2021-02-01 16:05:44 -0300

    fix

--- pages/api/home.js
+++ pages/api/home.js
@@ -2,5 +2,5 @@
 
 export default (req, res) => {
   // console.log(req)
-  res.status(200).json({ name: 'Teste de api com next.js' })
+  res.status(200).json({ message: 'Teste de api com next.js' })
 }
