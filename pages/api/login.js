f8d1d88190abb2aa0b7778595a5f20426d15fb94
heldinho <helderferreirapassos@gmail.com>
2021-02-15 15:53:07 -0200

    'fix'

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken')
 
 export default (req, res) => {
   // res.status(200).json({
-  //   message: 'Teste de api com next.js',
   //   headers: { ...req.headers },
   //   query: { ...req.query },
   //   body: { ...req.body }
@@ -15,7 +14,6 @@ export default (req, res) => {
     const token = jwt.sign({ id }, secret, {
       expiresIn: 300 /// expires in 5 min
     })
-    console.log(secret)
     return res.status(200).json({ auth: true, token: token })
   }
 
